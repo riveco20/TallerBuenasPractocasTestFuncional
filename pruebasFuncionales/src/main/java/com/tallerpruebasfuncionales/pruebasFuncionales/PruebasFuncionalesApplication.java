d58a8720f38153f10d2ad246a9378ad46bfc8cc2
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-10-17 12:11:48 -0500

    Montando Archivo Inicial

---
+++ pruebasFuncionales/src/main/java/com/tallerpruebasfuncionales/pruebasFuncionales/PruebasFuncionalesApplication.java
@@ -0,0 +1,13 @@
+package com.tallerpruebasfuncionales.pruebasFuncionales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasFuncionalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebasFuncionalesApplication.class, args);
+	}
+
+}
---
+++ pruebasFuncionales/src/test/java/com/tallerpruebasfuncionales/pruebasFuncionales/PruebasFuncionalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tallerpruebasfuncionales.pruebasFuncionales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasFuncionalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
